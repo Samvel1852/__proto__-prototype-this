f23025c0b9a8a199c2f8d4d25bba741d7e1b6d03
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-21 16:05:42 +0400

    feat: Added compareAge function for Person class

--- js/person.js
+++ js/person.js
@@ -8,11 +8,41 @@ class Person {
     this.name = name;
     this.age = age;
   }
+  compareAge(p) {
+    if (this.age === p.age) {
+      return this.name + " is the same age as " + p.name;
+    } else if (this.age > p.age) {
+      return this.name + " is older than " + p.name;
+    } else if (this.age < p.age) {
+      return this.name + " is younger than " + p.name;
+    }
+  }
 }
 
-const p1 = Person("Samuel", 24);
-const p2 = Person("Joel", 36);
-const p3 = Person("Lily", 24);
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// function compareAge(name, age) {
+//   if (this.age === age) {
+//     return this.name + "is the same age as" + name;
+//   } else if (this.age > age) {
+//     return this.name + "is older than" + name;
+//   } else if (this.age < age) {
+//     return this.name + "is younger than" + name;
+//   }
+// }
+
+// compareAge.prototype = Person;
+
+const p1 = new Person("Samuel", 24);
+const p2 = new Person("Joel", 36);
+const p3 = new Person("Lily", 24);
+
+console.log(p1, p2, p3);
+
+console.log(p1.compareAge(p2));
 // p1.compareAge(p2) ➞ "Joel is older than me."
 // p2.compareAge(p1) ➞ "Samuel is younger than me."
 // p1.compareAge(p3) ➞ "Lily is the same age as me."
